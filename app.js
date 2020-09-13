98a500043bc8b98b0cddb71795222450a91c40b2
Yevhenii Kolisnyk <YevheniiKolisnyk@users.noreply.github.com>
2020-09-13 07:05:59 +0200

    added .htaccess

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.use('/api/contact', contactRoute)
 app.use('/api/todo', todoRoute)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/dist/client'))
+  app.use(express.static('client/dist/generator'))
 
   app.get('*', (req, res) => {
     res.sendFile(
-        path.resolve(__dirname, 'client', 'dist', 'client', 'index.html')
+        path.resolve(__dirname, 'client', 'dist', 'generator', 'index.html')
     )
   })
 }
